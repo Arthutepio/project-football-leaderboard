389eca0a8ddee440ca04245c10885f932f3d1c57
arthutepio <arthurodriguesr10@gmail.com>
2023-03-15 13:20:17 -0300

    att12

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -4,7 +4,7 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.MYSQLUSER || 'root', //DB_USER
   password: process.env.MYSQLPASSWORD || '123456', //DB_PASS
-  database: 'TRYBE_FUTEBOL_CLUBE',
+  database: process.env.MYSQLDATABASE || 'TRYBE_FUTEBOL_CLUBE',
   host: process.env.MYSQLHOST || 'localhost', //DB_HOST
   port: Number(process.env.MYSQLPORT) || 3002, //DB_PORT
   dialect: 'mysql',
