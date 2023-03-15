36f659811495bb9028f5422a16a5be7262dfc3f6
arthutepio <arthurodriguesr10@gmail.com>
2023-03-15 10:51:42 -0300

    att6

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+const HOST = process.env.REACT_APP_API_PORT || 'localhost';
+const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || 'http';
 
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
+  baseURL: `${PROTOCOL}://${HOST}:3001`,
 });
 
 export const setToken = (token) => {
